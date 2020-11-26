74b5fa5be896cf395e5194cbf6f39f0d69dd4f00
kallevo <kalle.voutilainen@metropolia.fi>
2020-11-26 13:49:09 +0200

    Taide sivulle tehty dom, joka lisää sivulle kuvat. Tehty myös vähän css koodia taide sivulle.

--- js/taide.js
+++ js/taide.js
@@ -1,7 +1,27 @@
 //Kuvataulukko
 const kuvataulukko = [
-    
-]
+  {
+    'tiedostonimi': 'img/Jäkälä.jpg',
+  },
+  {
+    'tiedostonimi': 'img/Keltavahvero.jpg',
+  },
+  {
+    'tiedostonimi': 'img/Meri.jpg',
+  },
+  {
+    'tiedostonimi': 'img/Mustikka.jpg',
+  },
+  {
+    'tiedostonimi': 'img/Ruska.jpg',
+  },
+  {
+    'tiedostonimi': 'img/Havunneulaset.jpg',
+  },
+  {
+    'tiedostonimi': 'img/Havunneulaset.jpg',
+  },
+];
 
 
 
@@ -11,5 +31,19 @@ const kuvataulukko = [
 
 //Etsitään main tägi html koodista.
 let maini = document.querySelector('main');
+//Katsotaan, löytyykö main elementtiä.
+if (maini != null) {
+  console.log("Elementit löytyi!");
+}else {
+  console.log("Jotain meni pieleen.");
+}
+//Käydään taulukko läpi ja viedään kuvat taulukosta sivulle.
+for (let i = 0; i < kuvataulukko.length; i++) {
+  maini.innerHTML +=
+      `
+      <img src = ${kuvataulukko[i].tiedostonimi} alt = "kuva" >
+      
+      `;
+}
 
 
