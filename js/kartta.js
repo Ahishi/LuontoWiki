a3ae0dd4aa4f2217d6c18f29ee94780001235c5d
kallevo <kalle.voutilainen@metropolia.fi>
2020-12-04 12:38:17 +0200

    Lisätty tietoja sivulle video ja äänitteet.

--- js/kartta.js
+++ js/kartta.js
@@ -1,5 +1,6 @@
+//Etsitään kartta elementti kartta.html tiedostosta ja lisätään siihen elementtiin kartta. Kartalle asetetaan koordinaatit ja zoom taso.
 let kartta = L.map('kartta').setView([65.103,24.785], 5);
-
+//Lisätään kartalle ominaisuuksia.
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'map data &copy; <a href ="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
